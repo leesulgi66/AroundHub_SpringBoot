dd8174f38deba9eb775c71a19ed5f05e8987f201
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2023-05-14 16:05:49 +0900

    ProductEntity

---
+++ src/main/java/com/example/aroundhub/data/entity/ProductEntity.java
@@ -0,0 +1,31 @@
+package com.example.aroundhub.data.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "product")
+public class ProductEntity {
+
+    @Id
+    String productId;
+    String productName;
+    Integer productPrice;
+    Integer productStock;
+
+    /*
+    @Colum
+    String sellerId;
+
+    @Colum
+    String sellerPhoneNumber;
+     */
+}
