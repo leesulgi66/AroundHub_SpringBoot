7a3ecac499e22df77b65b94147a512a6cd87e2bf
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2023-05-14 16:06:31 +0900

    ProductDAOImpl

---
+++ src/main/java/com/example/aroundhub/data/dao/impl/ProductDAOImpl.java
@@ -0,0 +1,29 @@
+package com.example.aroundhub.data.dao.impl;
+
+import com.example.aroundhub.data.dao.ProductDAO;
+import com.example.aroundhub.data.entity.ProductEntity;
+import com.example.aroundhub.data.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ProductDAOImpl implements ProductDAO {
+
+    ProductRepository productRepository;
+
+    @Autowired
+    public ProductDAOImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public ProductEntity saveProduct(ProductEntity productEntity) {
+        productRepository.save(productEntity);
+        return productEntity;
+    }
+
+    @Override
+    public ProductEntity getProduct(String productId) {
+        ProductEntity productEntity = productRepository.getById(productId);
+        return productEntity;
+    }
+
+}
