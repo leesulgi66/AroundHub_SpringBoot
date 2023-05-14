953c59b6b19b862be60d8a8ff819fe9607ff0b3b
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2023-05-14 16:06:21 +0900

    ProductDAO

---
+++ src/main/java/com/example/aroundhub/data/dao/ProductDAO.java
@@ -0,0 +1,10 @@
+package com.example.aroundhub.data.dao;
+
+import com.example.aroundhub.data.entity.ProductEntity;
+
+public interface ProductDAO {
+
+    ProductEntity saveProduct(ProductEntity productEntity);
+
+    ProductEntity getProduct(String productId);
+}
