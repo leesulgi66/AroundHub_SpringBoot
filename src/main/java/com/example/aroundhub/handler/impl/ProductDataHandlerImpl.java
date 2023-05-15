16a4836d3c19d4e1b897e4a4a66ae69d1cab9579
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2023-05-15 17:04:20 +0900

    ProductDataHandlerImpl

---
+++ src/main/java/com/example/aroundhub/handler/impl/ProductDataHandlerImpl.java
@@ -0,0 +1,33 @@
+package com.example.aroundhub.handler.impl;
+
+import com.example.aroundhub.data.dao.ProductDAO;
+import com.example.aroundhub.data.entity.ProductEntity;
+import com.example.aroundhub.handler.ProductDataHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class ProductDataHandlerImpl implements ProductDataHandler {
+
+    ProductDAO productDAO;
+
+    @Autowired
+    public ProductDataHandlerImpl(ProductDAO productDAO) {
+        this.productDAO = productDAO;
+    }
+
+    @Override
+    public ProductEntity saveProductEntity(String productId, String productName, int productPrice, int productStock) {
+        ProductEntity productEntity = new ProductEntity(productId, productName, productPrice, productStock);
+
+        return productDAO.saveProduct(productEntity);
+    }
+
+    @Override
+    public ProductEntity getProductEntity(String productId) {
+        return productDAO.getProduct(productId);
+    }
+}
