f23b79182d0514820dc57c508b190bb7f72def2a
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2023-05-17 04:52:33 +0900

    logback test

--- src/main/java/com/example/aroundhub/controller/HelloController.java
+++ src/main/java/com/example/aroundhub/controller/HelloController.java
@@ -1,14 +1,29 @@
 package com.example.aroundhub.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class HelloController {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
 
     //@RequestMapping(value = "hello", method = RequestMethod.GET)  // 고전적인 방법으로 사용하지 않음.
     @GetMapping("hello")
     public String hello() {
         return "Hello World";
+    }
+
+    @PostMapping("log-test")
+    public void logTest(){
+
+        LOGGER.trace("Trace Log");
+        LOGGER.debug("debug Log");
+        LOGGER.info("info Log");
+        LOGGER.warn("warn Log");
+        LOGGER.error("error Log");
     }
 }
--- src/main/java/com/example/aroundhub/controller/ProductController.java
+++ src/main/java/com/example/aroundhub/controller/ProductController.java
@@ -2,6 +2,8 @@ package com.example.aroundhub.controller;
 
 import com.example.aroundhub.data.dto.ProductDto;
 import com.example.aroundhub.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/product-api")
 public class ProductController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
     private ProductService productService;
 
     @Autowired
@@ -19,7 +22,16 @@ public class ProductController {
     // http://localhost:8080/api/v1/product-api/product/{productId}
     @GetMapping(value = "/product/{productId}")
     public ProductDto getProduct(@PathVariable String productId) {
-        return productService.getProduct(productId);
+
+        long startTime = System.currentTimeMillis();
+        LOGGER.info("[ProductController] perform {} of Around Hub API.", "getProduct");
+
+        ProductDto productDto = productService.getProduct(productId);
+
+        LOGGER.info("[ProductController] Response :: productId = {}, productName = {}, productPrice = {}, productStock = {}, Response Time = {}ms", productId,
+                productDto.getProductName(), productDto.getProductPrice(), productDto.getProductStock(), (System.currentTimeMillis()-startTime));
+
+        return productDto;
     }
 
     // http://localhost:8080/api/v1/product-api/product
