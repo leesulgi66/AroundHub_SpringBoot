1a8cea0569a817f3b2307085edd6d34e1f5ba323
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2023-05-18 18:00:09 +0900

    ProductController - validation

--- src/main/java/com/example/aroundhub/controller/ProductController.java
+++ src/main/java/com/example/aroundhub/controller/ProductController.java
@@ -5,7 +5,11 @@ import com.example.aroundhub.service.ProductService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/product-api")
@@ -36,14 +40,27 @@ public class ProductController {
 
     // http://localhost:8080/api/v1/product-api/product
     @PostMapping(value = "/product")
-    public ProductDto createProduct(@RequestBody ProductDto productDto) {
+    public ResponseEntity<ProductDto> createProduct(@Valid @RequestBody ProductDto productDto) {    // @Valid 어노테이션으로 validation 활성화
+
+        // Validation Code Example
+        if (productDto.getProductId().equals("") || productDto.getProductId().isEmpty()) {
+            LOGGER.error("[createProduct] failed Response :: productId is Empty");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(productDto);
+        }
 
         String productId = productDto.getProductId();
         String productName = productDto.getProductName();
         int productPrice = productDto.getProductPrice();
         int productStock = productDto.getProductStock();
 
-        return productService.saveProduct(productId, productName, productPrice, productStock);
+        ProductDto response = productService.saveProduct(productId, productName, productPrice, productStock);
+
+        LOGGER.info(
+                "[createProduct] Response >> productId : {},productName : {}, productPrice : {}, productStock : {}",
+                response.getProductId(), response.getProductName(), response.getProductPrice(), response.getProductStock()
+        );
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     // http://localhost:8080/api/v1/product-api/product/{productId}
