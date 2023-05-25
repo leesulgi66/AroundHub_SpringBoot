3760570eb3cd42edf2fbc74b3f25199ab3336a7e
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2023-05-25 18:40:37 +0900

    ShortUrlResponseDto

---
+++ src/main/java/com/example/aroundhub/data/dto/ShortUrlResponseDto.java
@@ -0,0 +1,21 @@
+package com.example.aroundhub.data.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class ShortUrlResponseDto {
+
+    private String orgUrl;
+
+    private String shortUrl;
+
+}
