daf16876a8ae4572c5475ba0fdb53317816863dc
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2023-05-30 06:39:31 +0900

    ShortUrlServiceImpl - delete method

--- src/main/java/com/example/aroundhub/service/impl/ShortUrlServiceImpl.java
+++ src/main/java/com/example/aroundhub/service/impl/ShortUrlServiceImpl.java
@@ -93,13 +93,24 @@ public class ShortUrlServiceImpl implements ShortUrlService {
     }
 
     @Override
-    public ShortUrlResponseDto deleteByShortUrl(String shortUrl) {
-        return null;
+    public void deleteShortUrl(String url) {
+        if(url.contains("me2.do")) {
+            LOGGER.info("[deleteShortUrl] Request Url is 'ShortUrl'.");
+            deleteByShortUrl(url);
+        }else{
+            LOGGER.info("[deleteShortUrl] Request Url is 'OriginalUrl'.");
+            deleteByOriginalUrl(url);
+        }
     }
 
-    @Override
-    public ShortUrlResponseDto deleteByOriginalUrl(String originalUrl) {
-        return null;
+    private void deleteByShortUrl(String url) {
+        LOGGER.info("[deleteByShortUrl] delete record");
+        shortUrlDAO.deleteByShortUrl(url);
+    }
+
+    private void deleteByOriginalUrl(String url) {
+        LOGGER.info("[deleteByOriginalUrl] delete record");
+        shortUrlDAO.deleteByOriginalUrl(url);
     }
 
     private ResponseEntity<NaverUriDto> requestShortUrl(String clientId, String clientSecret,
