69a85f130ffd076a8fbf382414a76a53177d8355
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2023-05-31 19:15:34 +0900

    ListenerRepository

---
+++ src/main/java/com/example/aroundhub/data/repository/ListenerRepository.java
@@ -0,0 +1,8 @@
+package com.example.aroundhub.data.repository;
+
+import com.example.aroundhub.data.entity.ListenerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ListenerRepository extends JpaRepository<ListenerEntity, Long> {
+
+}
