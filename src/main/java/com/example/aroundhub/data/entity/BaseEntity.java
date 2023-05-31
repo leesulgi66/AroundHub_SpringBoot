eb8ba13362c9806926253b13a8c8c494ba05891f
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2023-05-31 18:41:29 +0900

    Annotation

--- src/main/java/com/example/aroundhub/data/entity/BaseEntity.java
+++ src/main/java/com/example/aroundhub/data/entity/BaseEntity.java
@@ -13,24 +13,24 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Getter
 @Setter
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
+@EntityListeners(AuditingEntityListener.class) // 엔티티 객체를 데이터베이스에 적용하기 전/후에 콜백을 요청하는 어노테이션
 public class BaseEntity {
 
-    @CreatedDate
+    @CreatedDate // 엔티티가 저장되는 시점에 자동으로 시간 주입
     @Column(updatable = false)
     private LocalDateTime createdAt;
 
   /*
-  @CreatedBy
+  @CreatedBy // 엔티티가 저장되는 시점에 저장 주체가 누구인지 주입
   @Column(updatable = false)
   private String createdBy;
   */
 
-    @LastModifiedDate
+    @LastModifiedDate // 엔티티가 수정되는 시점에 자동으로 시간을 주입
     private LocalDateTime updatedAt;
 
   /*
-  @LastModifiedBy
+  @LastModifiedBy // 엔티티가 수정되는 시점에 수정 주체가 누구인지 주입
   private String updatedBy;
   */
 
