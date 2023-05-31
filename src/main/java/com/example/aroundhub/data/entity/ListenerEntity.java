18c264522da14a0c2753a78b49bf3e42450b61b6
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2023-05-31 19:16:00 +0900

    ListenerEntity

---
+++ src/main/java/com/example/aroundhub/data/entity/ListenerEntity.java
@@ -0,0 +1,32 @@
+package com.example.aroundhub.data.entity;
+
+import com.example.aroundhub.data.entity.listener.CustomListener;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "listener")
+@EntityListeners(CustomListener.class)
+public class ListenerEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+}
