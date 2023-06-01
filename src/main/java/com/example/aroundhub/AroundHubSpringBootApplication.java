6d3eca9f514b641cbbf78de2b6006024db833edf
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2023-06-01 15:15:35 +0900

    @EnableJpaAuditing disable

--- src/main/java/com/example/aroundhub/AroundHubSpringBootApplication.java
+++ src/main/java/com/example/aroundhub/AroundHubSpringBootApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing	// JAP에서 AUditing은 각 엔티티 별로 누가, 언제 접근했는지 기록하여 감시 체계를 꾸리는 것을 의미
+//@EnableJpaAuditing	// JAP에서 AUditing은 각 엔티티 별로 누가, 언제 접근했는지 기록하여 감시 체계를 꾸리는 것을 의미 // -> Configuration class 생성을 통해서도 가능하다.(JpaAuditingConfiguration)
 public class AroundHubSpringBootApplication {
 
 	public static void main(String[] args) {
