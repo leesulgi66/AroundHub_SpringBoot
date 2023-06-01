18055a50a1c8380091e8acd8643a50017ef8be8b
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2023-06-01 15:15:49 +0900

    JpaAuditingConfiguration

---
+++ src/main/java/com/example/aroundhub/config/JpaAuditingConfiguration.java
@@ -0,0 +1,10 @@
+package com.example.aroundhub.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfiguration {
+
+}
