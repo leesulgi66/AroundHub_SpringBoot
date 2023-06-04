ec0d1bffdef5a4e9eef5b34008d025ff60cd6607
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2023-06-04 20:48:16 +0900

    ProductRepositoryTest

---
+++ src/test/java/com/example/aroundhub/data/repository/ProductRepositoryTest.java
@@ -0,0 +1,58 @@
+package com.example.aroundhub.data.repository;
+
+import com.example.aroundhub.data.entity.ProductEntity;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProductRepositoryTest {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @BeforeEach
+    void GenerateData() {
+        int count = 1;
+        productRepository.save(getProduct(Integer.toString(count), count++, 2000, 3000));
+        productRepository.save(getProduct(Integer.toString(count), count++, 3000, 9000));
+        productRepository.save(getProduct(Integer.toString(--count), count = count + 2, 1500, 200));
+        productRepository.save(getProduct(Integer.toString(count), count++, 4000, 5000));
+        productRepository.save(getProduct(Integer.toString(count), count++, 10000, 1500));
+        productRepository.save(getProduct(Integer.toString(count), count++, 1000, 1000));
+        productRepository.save(getProduct(Integer.toString(count), count++, 500, 10000));
+        productRepository.save(getProduct(Integer.toString(count), count++, 8500, 3500));
+        productRepository.save(getProduct(Integer.toString(count), count++, 7200, 2000));
+        productRepository.save(getProduct(Integer.toString(count), count++, 5100, 1700));
+    }
+
+    private ProductEntity getProduct(String id, int nameNumber, int price, int stock) {
+        return new ProductEntity(id, "상품" + nameNumber, price, stock);
+    }
+
+    @Test
+    void findTest() {
+        List<ProductEntity> foundAll = productRepository.findAll();
+        System.out.println("====↓↓ Test Data ↓↓====");
+        for(ProductEntity productEntity : foundAll) {
+            System.out.println(productEntity.toString());
+        }
+        System.out.println("====↑↑ Test Data ↑↑====");
+
+        List<ProductEntity> foundEntities = productRepository.findByProductName("상품4");
+
+        for(ProductEntity productEntity : foundEntities) {
+            System.out.println(productEntity.toString());
+        }
+
+        List<ProductEntity> queryEntities = productRepository.queryByProductName("상품4");
+
+        for(ProductEntity productEntity : queryEntities) {
+            System.out.println(productEntity.toString());
+        }
+
+    }
+
+}
