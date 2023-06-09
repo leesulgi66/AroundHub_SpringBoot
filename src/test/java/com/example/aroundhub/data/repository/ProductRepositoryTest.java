2a1af29fdbf920dc6866504fd9bfb88864f48e0f
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2023-06-09 16:08:10 +0900

    ProductRepositoryTest - parameterNamingQueryTest

--- src/test/java/com/example/aroundhub/data/repository/ProductRepositoryTest.java
+++ src/test/java/com/example/aroundhub/data/repository/ProductRepositoryTest.java
@@ -331,4 +331,19 @@ public class ProductRepositoryTest {
             System.out.println(productEntity);
         }
     }
+
+    @Test
+    public void parameterNamingQueryTest() {
+        List<ProductEntity> foundAll = productRepository.findAll();
+        System.out.println("====↓↓ Test Data ↓↓====");
+        for(ProductEntity productEntity : foundAll) {
+            System.out.println(productEntity.toString());
+        }
+        System.out.println("====↑↑ Test Data ↑↑====");
+
+        List<ProductEntity> foundProducts = productRepository.findByProductPriceWithParameterNaming(5000);
+        for(ProductEntity productEntity : foundProducts) {
+            System.out.println(productEntity);
+        }
+    }
 }
