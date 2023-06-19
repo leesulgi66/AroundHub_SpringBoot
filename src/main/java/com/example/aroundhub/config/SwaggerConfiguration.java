f5b45738e28f922c2fae3c00f8a909420ea0d7dd
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2023-06-19 20:59:52 +0900

    SwaggerConfiguration

--- src/main/java/com/example/aroundhub/config/SwaggerConfiguration.java
+++ src/main/java/com/example/aroundhub/config/SwaggerConfiguration.java
@@ -2,22 +2,46 @@ package com.example.aroundhub.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
 
     // http://localhost:8080/swagger-ui/#/
+
+    private static final String API_NAME = "Programmers Spring Boot Application";
+    private static final String API_VERSION = "1.0.0";
+    private static final String API_DESCRIPTION = "프로그래머스 스프링 부트 애플리케이션입니다.";
+
     @Bean
-    public Docket api(){
+    public Docket api() {
+        Parameter parameterBuilder = (Parameter) new ParameterBuilder()
+                .name(HttpHeaders.AUTHORIZATION)
+                .description("Access Tocken")
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")
+                .required(false)
+                .build();
+
+        List<Parameter> globalParameters = new ArrayList<>();
+        globalParameters.add(parameterBuilder);
+
         return new Docket(DocumentationType.SWAGGER_2)
+                .globalOperationParameters(globalParameters)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.example.aroundhub"))
@@ -25,11 +49,11 @@ public class SwaggerConfiguration {
                 .build();
     }
 
-    private ApiInfo apiInfo() {
+    public ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Around Hub Open API Test with Swagger")
-                .description("설명 부분")
-                .version("1.0")
+                .title(API_NAME)
+                .version(API_VERSION)
+                .description(API_DESCRIPTION)
                 .build();
     }
 }
